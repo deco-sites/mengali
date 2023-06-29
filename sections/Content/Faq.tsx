8e93ef1643db6ff99b3cf24f3bd074dea317eee8
vagnermengali <vagmengali2015@gmail.com>
2023-06-29 10:14:57 -0300

    feat: add new backup

--- sections/Content/Faq.tsx
+++ sections/Content/Faq.tsx
@@ -54,12 +54,12 @@ const DEFAULT_PROPS = {
 
 function Question({ question, answer }: Question) {
   return (
-    <details class="collapse collapse-arrow join-item border-t border-base-200">
-      <summary class="collapse-title text-lg font-medium">
+    <details class="collapse collapse-arrow join-item border-t border-[#CACACA] rounded-[0px]">
+      <summary class="collapse-title text-lg text-[#1C1C1E] uppercase font-black">
         {question}
       </summary>
       <div
-        class="collapse-content"
+        class="collapse-content text-[#3A3A3C] font-medium"
         dangerouslySetInnerHTML={{ __html: answer }}
       />
     </details>
